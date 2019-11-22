961258fd4118be27c786c433b0852fd4009ec77b
MadHowl <madhowl@yandex.ru>
2019-11-22 14:23:59 +1000

    Подключаем Eloqent

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends BaseController
 
     public function index($request,$response)
     {
+        
         return $this->container->view->render($response, 'home.twig');
     }
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,31 @@ require __DIR__ .'/../vendor/autoload.php';
 $app = new \Slim\App([
     'settings' =>[
         'displayErrorDetails'=>true,
-    ]
+        'db' => [
+            'driver' => 'mysql',
+            'host' => '192.168.200.79',
+            'database' => 'slim-auth',
+            'username' => 'user',
+            'password' => 'user',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci',
+            'prefixs' =>'',
+        ]
+    ],
+
  ]);
 
 $container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule){
+    return $capsule;
+};
+
 $container['view'] = function ($container){
     $view = new \Slim\Views\Twig(__DIR__.'/../resources/views',[
         'cache' => false,
