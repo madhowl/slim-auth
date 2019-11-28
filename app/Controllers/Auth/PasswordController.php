0442d4702e12fbfbc6c9e8051289addf2f9e9bbd
MadHowl <madhowl@yandex.ru>
2019-11-28 16:24:17 +1000

    Валидация старого пароля

--- app/Controllers/Auth/PasswordController.php
+++ app/Controllers/Auth/PasswordController.php
@@ -17,6 +17,16 @@ class PasswordController extends BaseController
     }
     public function postChangePassword($request, $response)
     {
+        $validation = $this->validator->validate($request, [
+
+            'password_old' => v::noWhitespace()->notEmpty()->matchesPassword($this->auth->user()->password),
+
+            'password' => v::noWhitespace()->notEmpty(),
+
+        ]);
+        if ($validation->failed()){
+            return $response->withRedirect($this->router->pathFor('auth.password.change'));
+        }
 
     }
 }
